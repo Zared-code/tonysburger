8f0c26446a9730a7d5a5a7f61bc5e0f81869b628
Tony Mascate <tonymascate@hotmail.com>
2021-01-29 18:25:36 +0100

    init

--- JS/app.js
+++ JS/app.js
@@ -170,3 +170,31 @@ if (matchMedia) {
   }
   
   }
+
+
+  /* HEADER */
+const hamburger = document.querySelector('.header .navbar .navbar__list .hamburger');
+const mobile_menu = document.querySelector('.header .navbar .navbar__list ul');
+const menu_item = document.querySelectorAll('.header .navbar .navbar__list ul li a');
+const header = document.querySelector('.header.container');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    mobile_menu.classList.toggle('active');
+});
+
+document.addEventListener('scroll', () => {
+    var scroll_position = window.scrollY;
+    if (scroll_position > 150) {
+        header.style.backgroundColor = '#29323c';
+    } else {
+        header.style.backgroundColor = 'transparent';
+    }
+});
+
+menu_item.forEach(item => {
+    item.addEventListener('click', () => {
+        hamburger.classList.toggle('active');
+        mobile_menu.classList.toggle('active');
+    })
+})
